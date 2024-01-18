fb4bc0946647bda59605bcca0e45144d92f4ce75
Iyanu <tobiy23@gmail.com>
2024-01-18 23:18:47 +0100

    update

--- _push.c
+++ _push.c
@@ -6,9 +6,16 @@
  * @line_number: the line of the command on the file
  */
 
-void push(stack_t **stack, unsigned int line_number __attribute__((unused)))
+void push(stack_t **stack, unsigned int line_number)
 {
 	char *n = global.argument;
+
+	if (isdigit(n[0]) == 0)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+		return;
+	}
 
 	create_node(stack, atoi(n));
 }
