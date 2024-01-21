931422de7ce065d99e2867d4bb3453d9b404741f
Iyanu <tobiy23@gmail.com>
2024-01-21 20:23:56 +0100

    add pchar

--- pchar.c
+++ pchar.c
@@ -20,7 +20,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 
 	if (iscii == 0)
 	{
-		fprintf(stderr, "L%u: can't pchar, value out of range", line_number);
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
